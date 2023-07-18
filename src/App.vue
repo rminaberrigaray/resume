5790ed60b70af0c0350d13a888c2819d015615cd
Rodrigo Minaberrigaray <rodrigo.minaberrigaray@gmail.com>
2023-07-18 00:00:23 -0300

    photo removed + education + about me

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,7 @@
     <!-- Page -->
     <div class="flex flex-col mx-auto max-w-a4 md:h-a4 print:h-a4 print:w-a4 bg-white shadow-[0_0_8px_rgba(13,12,12,0.15)] print:shadow-none">
       <!-- Name -->
-      <header class="p-8 md:h-[4.7cm] print:h-[4.7cm] flex items-center space-x-10 bg-slate-700">
-        <img class="h-full rounded-full" alt="Profile picture" src="./assets/images/profile.jpg">
+      <header class="p-8 md:h-[3.7cm] print:h-[3.7cm] flex items- justify-center space-x-10 bg-slate-700">
         <div class="space-y-1">
           <h1 class="text-5xl font-extralight tracking-wide text-white text-center">
             Rodrigo Minaberrigaray
@@ -23,13 +22,13 @@
           </h2>
         </div>
       </header>
-      <div class="flex flex-1 w-full h-full">
-        <div class=" basis-2/5 px-4 py-3 bg-slate-100">
+      <div class="flex flex-1 w-full h-full text-slate-700">
+        <div class="basis-2/5 px-4 py-3 space-y-6 bg-slate-100">
           <section class="space-y-2">
-            <h3 class="text-xl font-medium uppercase text-slate-700 border-b border-slate-400">
+            <h3 class="text-xl font-medium uppercase border-b border-slate-400">
               Contacto
             </h3>
-            <ul class="text-slate-700 text-sm space-y-2 lining-nums">
+            <ul class="text-sm space-y-2 lining-nums">
               <li class="flex items-center space-x-2">
                 <EnvelopeIcon class="w-5 h-5" />
                 <a href="mailto:rodrigo.minaberrigaray@gmail.com">
@@ -48,8 +47,60 @@
               </li>
             </ul>
           </section>
+          <section class="space-y-2">
+            <h3 class="text-xl font-medium uppercase border-b border-slate-400">
+              Estudios
+            </h3>
+            <div class="text-sm lining-nums">
+              <p class="font-semibold">
+                Analista Programador Universitario
+              </p>
+              <p class="font-medium">
+                2012-2018 | Universidad Nacional de La Plata
+              </p>
+            </div>
+            <div class="text-sm lining-nums">
+              <p class="font-semibold">
+                Licenciatura en Sistemas
+              </p>
+              <p class="font-medium">
+                2012-2019 | Universidad Nacional de La Plata
+              </p>
+              <p class="italic">Tesina pendiente</p>
+            </div>
+          </section>
+          <section class="space-y-2">
+            <h3 class="text-xl font-medium uppercase border-b border-slate-400">
+              Idiomas
+            </h3>
+            <ul class="text-sm list-disc list-inside">
+              <li class="">
+                Español: nativo
+              </li>
+              <li class="">
+                Inglés: nivel intermedio
+              </li>
+            </ul>
+          </section>
         </div>
-        <div class="basis-3/5"></div>
+        <div class="basis-3/5 px-4 py-3 space-y-6">
+          <section class="space-y-2">
+            <h3 class="text-xl font-medium uppercase border-b border-slate-400">
+              Presentación
+            </h3>
+            <p class="text-base">
+              Me considero una persona abierta y tolerante. Soy resolutivo y pragmático.
+              Además de mi rol como desarrollador, disfruto colaborando en el análisis de las soluciones.
+              Siempre con gran predispocición para las tareas que se requieran, ya sea aprender
+              nuevas tecnologías o realizar cambios sobre alguna aplicación legacy.
+            </p>
+          </section>
+          <section class="space-y-2">
+            <h3 class="text-xl font-medium uppercase border-b border-slate-400">
+              Experiencia
+            </h3>
+          </section>
+        </div>
       </div>
     </div>
   </main>
